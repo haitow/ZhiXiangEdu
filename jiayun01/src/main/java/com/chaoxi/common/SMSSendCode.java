4d8d32e8a8ff1c7c45cf37ba8a0048426d313c6e
haitow <haitide@126.com>
2026-03-30 22:32:22 +0800

    daily commit

--- jiayun01/src/main/java/com/chaoxi/common/SMSSendCode.java
+++ jiayun01/src/main/java/com/chaoxi/common/SMSSendCode.java
@@ -17,6 +17,7 @@ public class SMSSendCode {
 
   int dailyCommit1 = 1;
   int dailyCommit2 = 2;
+  int dailyCommit3 = 3;
 
   private static final String ACCESS_KEY_SECRET = System.getenv("temp2env");//秘钥
 
